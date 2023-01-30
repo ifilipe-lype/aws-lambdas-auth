bb34729103ffe19fea57d1ddc107178ca4275c70
ifilipe-lype <ifilipe.lype@gmail.com>
2023-01-30 21:22:00 +0100

    test: setup failed test

--- __tests__/integrations/tempTest.test.ts
+++ __tests__/integrations/tempTest.test.ts
@@ -2,4 +2,8 @@ describe("Temporary fake test, for testing setup", () => {
   it("Should pass", () => {
     expect(1 + 2).toBe(3);
   });
+
+  it("Should fail", () => {
+    expect(1 + 2).toBe(5);
+  });
 });
