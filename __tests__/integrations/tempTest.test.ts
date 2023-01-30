24638594e08de5572b7ae0bfdc910332c2c52a5c
ifilipe-lype <ifilipe.lype@gmail.com>
2023-01-30 21:28:00 +0100

    fix: removes failed test

--- __tests__/integrations/tempTest.test.ts
+++ __tests__/integrations/tempTest.test.ts
@@ -2,8 +2,4 @@ describe("Temporary fake test, for testing setup", () => {
   it("Should pass", () => {
     expect(1 + 2).toBe(3);
   });
-
-  it("Should fail", () => {
-    expect(1 + 2).toBe(5);
-  });
 });
